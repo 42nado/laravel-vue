7812248f8578e4b19455afb0ad6b81bbd206541b
Ironsick_ <daryllfortunado@gmail.com>
2024-03-23 00:25:42 +0800

    Commented out the route for viewing dashboard and added require statement for auth.php

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::view('/{any}', 'dashboard')
-    ->middleware(['auth'])
-    ->where('any', '.*');
+// Route::view('/{any}', 'dashboard')
+//     ->middleware(['auth'])
+//     ->where('any', '.*');
+
 require __DIR__.'/auth.php';
